ae3b0d2f5999b0b973d3fbf0f42f4de820389158
Andrey <69050126+andreymslkv@users.noreply.github.com>
2021-05-09 15:30:32 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,8 @@
+$(document).ready(function () {
+  $("#app").vide({
+    mp4: "video/video.mp4",
+    webm: "video/video.webm",
+    ogv: "vieo/video.ogv",
+    poster: "img/cover-image",
+  });
+});
